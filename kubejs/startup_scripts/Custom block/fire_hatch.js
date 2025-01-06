e8366c4589f6aa1da8dd6592a89729a2f0dc8c7d
ymna-ytka <rogozinskiyroza059@gmail.com>
2025-01-06 12:11:14 +0200

    Update fire_hatch.js
    disable fire hatch

--- kubejs/startup_scripts/Custom block/fire_hatch.js
+++ kubejs/startup_scripts/Custom block/fire_hatch.js
@@ -1,60 +1,49 @@
-const $ItemBusPartMachine = Java.loadClass('com.gregtechceu.gtceu.common.machine.multiblock.part.ItemBusPartMachine')
-
-const $FluidBusPartMachine = Java.loadClass('com.gregtechceu.gtceu.common.machine.multiblock.part.FluidHatchPartMachine')
-
-GTCEuStartupEvents.registry('gtceu:machine', event=>{
-
-    event.create("item_import_hatch_fire", "custom")
-        
-        .tiers(GTValues.ULV)
-        .machine((holder, tier, t)=>
-            new $ItemBusPartMachine)
-        .definition((tier, builder)=>{
-        
-            
-        builder.rotationState(RotationState.ALL)
-               
-
-        .workableTieredHullRenderer('item_bus.export')
-                
-        .abilities(PartAbility.IMPORT_ITEMS)
-    
-
-    })
-
-    event.create("item_export_hatch_fire", "custom")
-        
-    .tiers(GTValues.ULV)
-    .machine((holder, tier, t)=>
-        new $ItemBusPartMachine)
-    .definition((tier, builder)=>{
-    
-        
-    builder.rotationState(RotationState.ALL)
-           
-
-    .overlayTieredHullRenderer('item_bus.export')
-            
-    .abilities(PartAbility.EXPORT_ITEMS)
-
-
-})
-
-    event.create("fluid_input_hatch_fire", "custom")
-        
-        .tiers(GTValues.ULV)
-        .machine((holder, tier, t)=>
-            new $FluidBusPartMachine)
-        .definition((tier, builder)=>{
-        
-            
-        builder.rotationState(RotationState.ALL)
-               
-
-        .overlayTieredHullRenderer('fluid_hatch.import')
-        
-        .abilities(PartAbility.IMPORT_FLUIDS)
-
-    })
-
-    })
+//const $ItemBusPartMachine = Java.loadClass('com.gregtechceu.gtceu.common.machine.multiblock.part.ItemBusPartMachine')
+//
+//const $FluidBusPartMachine = Java.loadClass('com.gregtechceu.gtceu.common.machine.multiblock.part.FluidHatchPartMachine')
+//
+//GTCEuStartupEvents.registry('gtceu:machine', event=>{
+//    event.create("item_import_hatch_fire", "custom")
+//        
+//        .tiers(GTValues.ULV)
+//        .machine((holder, tier, t)=> new $ItemBusPartMachine)
+//        .definition((tier, builder)=>{
+//        builder.rotationState(RotationState.ALL)
+//               
+//
+//        .overlayTieredHullRenderer('item_bus.import')
+//                
+//        .abilities(PartAbility.IMPORT_ITEMS)
+//    
+//
+//    })
+//
+//    event.create("item_export_hatch_fire", "custom")
+//        
+//    .tiers(GTValues.ULV)
+//    .machine((holder, tier, t)=> new $ItemBusPartMachine)
+//    .definition((tier, builder)=>{
+//        builder.rotationState(RotationState.ALL)
+//           
+//
+//    .overlayTieredHullRenderer('item_bus.export')
+//            
+//    .abilities(PartAbility.EXPORT_ITEMS)
+//
+//
+//})
+//
+//    event.create("fluid_input_hatch_fire", "custom")
+//        
+//        .tiers(GTValues.ULV)
+//        .machine((holder, tier, t)=>new $FluidBusPartMachine)
+//        .definition((tier, builder)=>{
+//            builder.rotationState(RotationState.ALL)
+//               
+//
+//        .overlayTieredHullRenderer('fluid_hatch.import')
+//        
+//        .abilities(PartAbility.IMPORT_FLUIDS)
+//
+//    }
+//})
