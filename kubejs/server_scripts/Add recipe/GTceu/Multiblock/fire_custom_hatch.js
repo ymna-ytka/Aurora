92c349e606bc4c7a30fafd8e0434233c04c42885
ymna-ytka <rogozinskiyroza059@gmail.com>
2025-01-06 12:18:03 +0200

    disable craft fire hatch
    Відключив тимчасово повністю вогняні кастомні хетчі та баси

---
+++ kubejs/server_scripts/Add recipe/GTceu/Multiblock/fire_custom_hatch.js
@@ -0,0 +1,57 @@
+//ServerEvents.recipes(event=>{
+  //Fire hatch  
+//    event.shaped("gtceu:ulv_item_import_hatch_fire",
+//        
+//        [
+//            " O ",
+//            "HCW",
+//            " F " 
+//        ],
+//        
+//        {
+//            C:"kubejs:fire_casing",
+//            O:"minecraft:hopper",
+//            W:"#forge:tools/wrench",
+//            H:"#forge:tools/hammers",
+//            F:"#forge:tools/files"  
+//        }
+//     
+//    )
+//    
+//   event.shaped("gtceu:ulv_item_export_hatch_fire",
+//       
+//        [
+//            " F ",
+//            "HCW",
+//            " O " 
+//        ],
+//        
+//        {
+//            C:"kubejs:fire_casing",
+//            O:"minecraft:hopper",
+//            W:"#forge:tools/wrench",
+//            H:"#forge:tools/hammers",
+//            F:"#forge:tools/files"  
+//        }
+//     
+//    )
+//    
+//    event.shaped("gtceu:ulv_fluid_input_hatch_fire",
+//        
+//        [
+//            "B B",
+//            "HCW",
+//            " F " 
+//        ],
+//        
+//        {
+//            C:"kubejs:fire_casing",
+//            B:"fluidtank:tank_wood",
+//            W:"#forge:tools/wrench",
+//            H:"#forge:tools/hammers",
+//            F:"#forge:tools/files"  
+//        }
+//     
+//    )
+//
+//})
