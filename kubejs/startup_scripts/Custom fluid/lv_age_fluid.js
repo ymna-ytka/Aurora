97414680c53baf812aeaa7385ab24dba5d8cbd8e
ymna-ytka <rogozinskiyroza059@gmail.com>
2025-02-17 17:04:02 +0200

    add new gas and fluid

---
+++ kubejs/startup_scripts/Custom fluid/lv_age_fluid.js
@@ -0,0 +1,9 @@
+GTCEuStartupEvents.registry('gtceu:material', event => {
+
+    event.create('liquid_twilight_gas')
+
+        .liquid()
+        .color(0xb69e74)
+        .iconSet(GTMaterialIconSet.DULL)
+
+})    
---
+++ kubejs/startup_scripts/Custom gas/lv_age_gas.js
@@ -0,0 +1,9 @@
+GTCEuStartupEvents.registry('gtceu:material', event => {
+
+    event.create('twilight_gas')
+
+        .gas()
+        .color(0xb69e74)
+        .iconSet(GTMaterialIconSet.DULL)
+
+})    
