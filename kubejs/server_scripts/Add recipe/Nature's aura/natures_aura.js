3e7988e5f496a72f42caf758ebda4bd903f2eefe
ymna-ytka <rogozinskiyroza059@gmail.com>
2025-02-19 00:28:53 +0200

    add craft wood stand and add localization

---
+++ kubejs/server_scripts/Add recipe/Nature's aura/natures_aura.js
@@ -0,0 +1,18 @@
+ServerEvents.recipes( event=> {
+
+    event.shaped("naturesaura:wood_stand",
+
+        [
+            "W W",
+            "WWW",
+            " S "
+        ],
+
+        {
+            W:"#minecraft:logs",
+            S:"forge:tools/saws"
+        }
+
+    )
+
+})
