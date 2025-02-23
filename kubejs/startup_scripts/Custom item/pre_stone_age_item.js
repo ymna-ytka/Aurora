6a2a3c94ad65f0cc00327c3d0b6e87376b1978ff
ymna-ytka <rogozinskiyroza059@gmail.com>
2025-02-23 18:52:47 +0200

    add stone tools

--- kubejs/startup_scripts/Custom item/pre_stone_age_item.js
+++ kubejs/startup_scripts/Custom item/pre_stone_age_item.js
@@ -12,4 +12,23 @@ StartupEvents.registry('item', event=> {
     .displayName('Шматок дерев\'яної резини')
     .texture('kubejs:item/pre_stone_age/piece_wooden_rubber')
 
+    event.create('ingot_wood_mold')
+    .displayName('Дерев\'яна форма')
+    .texture('kubejs:item/pre_stone_age/ingot_wood_mold')
+
+    event.create('stone_ingot')
+    .displayName('Кам\'яний злиток')
+    .texture('kubejs:item/pre_stone_age/stone_ingot')
+
+    event.create('stone_plate')
+    .displayName('Кам\'яний пластина')
+    .texture('kubejs:item/pre_stone_age/stone_plate')
+
+    event.create('ingot_stone_mold')
+    .displayName('Кам\'яний форма')
+    .texture('kubejs:item/pre_stone_age/ingot_stone_mold')
+
+
+
+
 })
