429e9064559fa5560facb679613bf9b22883013b
ymna-ytka <rogozinskiyroza059@gmail.com>
2025-03-11 13:53:11 +0200

    Add elements
    Додані елементи до матеріалів

--- kubejs/startup_scripts/Custom element/lv_age.js
+++ kubejs/startup_scripts/Custom element/lv_age.js
@@ -3,6 +3,27 @@ GTCEuStartupEvents.registry('gtceu:element', event => {
     event.create('sodium_carbonate')
         .protons(78) 
         .neutrons(78) 
-        .symbol('Na₂CO₃') 
-     
+        .symbol('Na₂CO₃')
+    
+    event.create('vitalized_iron')
+        .protons(26) 
+        .neutrons(25) 
+        .symbol('Vi') 
+        
+    event.create('twilight_gold')
+        .protons(79) 
+        .neutrons(125) 
+        .symbol('Tg') 
+
+    event.create('cold_water')
+        .protons(78) 
+        .neutrons(78) 
+        .symbol('H₂O')
+
+    event.create('magical_essence')
+        .protons(78) 
+        .neutrons(78) 
+        .symbol('???')
+
+        
 })
