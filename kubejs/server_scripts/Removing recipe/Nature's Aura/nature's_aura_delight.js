b2a91095d4199b17cc9ebaa8fd8eddda82eff793
ymna-ytka <rogozinskiyroza059@gmail.com>
2025-04-10 18:01:24 +0300

    delete craft knife for mod naturesauradelight

---
+++ kubejs/server_scripts/Removing recipe/Nature's Aura/nature's_aura_delight.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes( event=> {
+
+    event.remove( { id:"naturesauradelight:infused_knife" } )
+    event.remove( { id:"naturesauradelight:depth_knife" } )
+    event.remove( { id:"naturesauradelight:sky_knife" } )
+    event.remove( { id:"naturesauradelight:tainted_knife" } )
+
+})
