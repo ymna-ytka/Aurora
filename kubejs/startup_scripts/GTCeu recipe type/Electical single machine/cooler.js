485fc6e3fad036618b384658c9b1985312d6604f
ymna-ytka <rogozinskiyroza059@gmail.com>
2025-04-10 17:53:04 +0300

    Change sound cooler
    Змінено звук роботи охолоджувача

--- kubejs/startup_scripts/GTCeu recipe type/Electical single machine/cooler.js
+++ kubejs/startup_scripts/GTCeu recipe type/Electical single machine/cooler.js
@@ -6,6 +6,6 @@ GTCEuStartupEvents.registry("gtceu:recipe_type", event=>{
         .setMaxIOSize(1, 1, 1, 1)
         .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
         .setProgressBar(GuiTextures.PROGRESS_BAR_GAS_COLLECTOR, FillDirection.LEFT_TO_RIGHT) 
-        .setSound(GTSoundEntries.MIXER)
+        .setSound(GTSoundEntries.COOLING)
 
 })
