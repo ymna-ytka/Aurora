ad685886c43e530c57019bfb85bd2a76d779ce0f
ymna-ytka <rogozinskiyroza059@gmail.com>
2025-04-10 17:57:54 +0300

    Add chemical formul for basic liquid fertilizer

---
+++ kubejs/startup_scripts/Custom element/mv_age.js
@@ -0,0 +1,9 @@
+GTCEuStartupEvents.registry('gtceu:element', event => {
+
+    event.create('basic_liquid_fertilizer')
+    .protons(78) 
+    .neutrons(78) 
+    .symbol('H₂ONH₄ClH₃PO₄K₂SO₄')
+
+
+})
